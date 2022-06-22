1cd87e70ed41c6ee03114015f57458fe8eb9ad4d
ioulosve <>
2022-06-22 12:23:26 +0200

    cnr read_dataset temporal/spatial Null handling

--- src/data_access/query_sios.py
+++ src/data_access/query_sios.py
@@ -248,10 +248,32 @@ def read_dataset_cnr(dataset_opendap_url, variables_list=[], temporal_extent=[No
   # No datasets found with this variables
   if erddap_vars == []:
     return None
-
-  endpoint = dataset_opendap_url
-  query = endpoint + f'?station_id,latitude,longitude,time,{",".join(erddap_vars)}&latitude>={spatial_extent[1]}&latitude<={spatial_extent[3]}&longitude>={spatial_extent[0]}&longitude<={spatial_extent[2]}&time>={temporal_extent[0]}&time<={temporal_extent[1]}'
   
+  endpoint = dataset_opendap_url
+  #query = endpoint + f'?station_id,latitude,longitude,time,{",".join(erddap_vars)}&latitude>={spatial_extent[1]}&latitude<={spatial_extent[3]}&longitude>={spatial_extent[0]}&longitude<={spatial_extent[2]}&time>={temporal_extent[0]}&time<={temporal_extent[1]}'
+  
+  query = endpoint + f'?station_id,latitude,longitude,time,{",".join(erddap_vars)}'
+
+  if temporal_extent[0] is not None:
+    query = query + f'&time>={temporal_extent[0]}'
+
+  if temporal_extent[1] is not None:
+    query = query + f'&time<={temporal_extent[1]}'
+  
+  if spatial_extent[0] is not None:
+    query = query + f'&longitude>={spatial_extent[0]}'
+
+  if spatial_extent[1] is not None:
+    query = query + f'&latitude>={spatial_extent[1]}'
+    
+  if spatial_extent[2] is not None:
+    query = query + f'&longitude<={spatial_extent[2]}'
+
+  if spatial_extent[3] is not None:
+    query = query + f'&latitude<={spatial_extent[3]}'
+
+  print(query)
+
   response = requests.get(query, verify=cnr_certificate_path)
 
   # No datasets for the query
@@ -348,4 +370,4 @@ if __name__ == "__main__":
   #print(get_list_platforms())
   #print(get_list_variables())
   #print(query_datasets(['Pressure (surface)', 'Ozone'], ['2009-09-20T00:00:00Z','2021-09-20T00:00:00Z'], [-22, 37, 52, 88]))
-  print(read_dataset('https://data.iadc.cnr.it/erddap/tabledap/cct_meteo_d2.nc', ['Pressure (surface)', 'Temperature (near surface)', 'Ozone' ],  ['2021-09-20T00:00:00Z','2022-09-20T00:00:00Z'], [-22, 37, 52, 88]))
+  print(read_dataset('https://data.iadc.cnr.it/erddap/tabledap/cct_meteo_d2.nc', ['Pressure (surface)', None],  [None,None], [None, None, None, None]))
